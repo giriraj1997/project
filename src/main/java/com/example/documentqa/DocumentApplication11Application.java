c8caca8544d52bb5d7a372971306b294e52ef8a1
giridharmaraj <girir8589@gmail.com>
2025-05-05 16:29:52 +0530

    updated

---
+++ src/main/java/com/example/documentqa/DocumentApplication11Application.java
@@ -0,0 +1,13 @@
+package com.example.documentqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentApplication11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentApplication11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/documentqa/DocumentApplication11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.documentqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentApplication11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
